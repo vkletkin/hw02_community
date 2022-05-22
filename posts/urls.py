24f38ba7a2d7c404523618225cef39ac74ac56c3
Vasiliy Kletkin <v.kletkin@mail.ru>
2022-05-22 23:15:37 +0300

    last

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, group_posts
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path ('group/<slug:slug>/', group_posts, name="group_posts")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Post
+# Create your views here.
+from django.http import HttpResponse
+from .models import Post, Group
+
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
-    return render(request, "index.html", {"posts": latest})
+    return render(request, "index.html", {"posts": latest}) 
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group":group, "posts":posts})
