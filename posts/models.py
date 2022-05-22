6f146da2c8a2b0843a6d1fa7ad73d1ca4c0046ec
Vasiliy Kletkin <v.kletkin@mail.ru>
2022-05-22 23:30:55 +0300

    ok

--- posts/models.py
+++ posts/models.py
@@ -18,3 +18,6 @@ class Post(models.Model):
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts") 
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, blank=True, null=True, related_name="posts") 
+
+    def __str__(self):
+        return self.title[:100]
